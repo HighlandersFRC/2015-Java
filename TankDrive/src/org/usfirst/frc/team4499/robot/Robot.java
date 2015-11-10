3e088397f68035f3a6eff43568c987ebd8a3aecd
infinite-void <jwiens3141@gmil.com>
2015-11-09 20:36:14 -0700

    Updated tank drive, now has shifters, NavX, Encoders

--- TankDrive/src/org/usfirst/frc/team4499/robot/Robot.java
+++ TankDrive/src/org/usfirst/frc/team4499/robot/Robot.java
@@ -1,37 +1,64 @@
+
 package org.usfirst.frc.team4499.robot;
 
 import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import edu.wpi.first.wpilibj.Joystick;
-
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.kauailabs.navx.frc.AHRS;
 
 public class Robot extends IterativeRobot {
+	AHRS ahrs = new AHRS(SerialPort.Port.kMXP);
 	CANTalon motorLeftOne = new CANTalon(1);
 	CANTalon motorLeftTwo = new CANTalon(2);
 	CANTalon motorRightOne = new CANTalon(3);
 	CANTalon motorRightTwo = new CANTalon(4);
 	RobotDrive myRobot;
-	Joystick stickLeft;
-	Joystick stickRight;
-	int autoLoopCounter;
+	Joystick controllerOne;
+	JoystickButton safetyOff;
+	JoystickButton safetyOn;
+	JoystickButton safetyOffTwo;
 	
+	JoystickButton pistonOneIn;
+	JoystickButton pistonOneOut;
+	DoubleSolenoid pistonOne;
+	int autoLoopCounter;
+	double safety = 0.5;
+	Value pistonValue = DoubleSolenoid.Value.kOff;
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {   	
     	myRobot = new RobotDrive(0,1);
-    	stickLeft = new Joystick(0);
-    	stickRight = new Joystick(0);
+    	controllerOne = new Joystick(0);
+    	safetyOff = new JoystickButton(controllerOne,7);
+    	safetyOffTwo = new JoystickButton(controllerOne,8);
+    	safetyOn = new JoystickButton(controllerOne,2);
+    	pistonOne = new DoubleSolenoid(0,1);
+    	pistonOneIn = new JoystickButton(controllerOne, 6);
+    	pistonOneOut = new JoystickButton(controllerOne,5);
+    	safety = 0.5;
     }
     
     /**
      * This function is run once each time the robot enters autonomous mode
      */
-    public void autonomousInit() {    	
+    public void autonomousInit(){
     	autoLoopCounter = 0;
     }
 
@@ -58,15 +85,32 @@ public class Robot extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-
-        //myRobot.arcadeDrive(stick);
-    	motorLeftOne.set(stickLeft.getRawAxis(1));
-    	motorLeftTwo.set(stickLeft.getRawAxis(1));
+    	if(safetyOn.get()){
+    		safety = 0.5;
+    	}
+    	if (safetyOff.get()&& safetyOffTwo.get()){
+    		safety = 1;
+    	}
+    	if(pistonOneIn.get()){
+    		pistonValue = DoubleSolenoid.Value.kForward;
+    	}
+    	if(pistonOneOut.get()){
+    		pistonValue = DoubleSolenoid.Value.kReverse;
+    	}
+    	//System.out.println("IMU_TotalYaw    " +ahrs.getAngle());
+    	System.out.print(motorRightOne.getEncPosition() + "  ");
+    	System.out.print(motorRightTwo.getEncPosition() + "  ");
+    	System.out.print(motorLeftOne.getEncPosition() + "  ");
+    	System.out.println(motorLeftTwo.getEncPosition());
     	
-    	motorRightOne.set(-stickRight.getRawAxis(5));
-    	motorRightTwo.set(-stickRight.getRawAxis(5));
-        
-    }
+    		pistonOne.set(pistonValue);
+	    	motorLeftOne.set(controllerOne.getRawAxis(1) * safety);
+	    	motorLeftTwo.set(controllerOne.getRawAxis(1) * safety);
+	    	
+	    	motorRightOne.set(-controllerOne.getRawAxis(5)* safety);
+	    	motorRightTwo.set(-controllerOne.getRawAxis(5)* safety);
+    	}
+       // System.out.println("Test");
     
     /**
      * This function is called periodically during test mode
